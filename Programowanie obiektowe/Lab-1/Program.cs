b670f81914c0736d1a6eb0c5fde87f017b6dd7de
JakubMizera <kubamiz53@gmail.com>
2022-03-11 17:26:37 +0100

    homework-1

--- Programowanie obiektowe/Lab-1/Program.cs
+++ Programowanie obiektowe/Lab-1/Program.cs
@@ -1,113 +1,163 @@
 ﻿using System;
 
-namespace Lab_1
+
+public enum Currency
 {
-    class Program
+    PLN = 1,
+    USD = 2,
+    RUB = 3
+}
+
+public class Money : IEquatable<Money>
+{
+    private readonly decimal _value;
+    private readonly Currency _currency;
+    private Money(decimal value, Currency currency)
     {
-        static void Main(string[] args)
-        {
-            //PersonProperties person = person.FirstName("asd");
-            //Console.WriteLine(person.FirstName);
-            DateTime day = DateTime.Parse("31-02-2022");
-        }
+        _value = value;
+        _currency = currency;
+    }
+    public override string ToString()
+    {
+        return $"{_value} {_currency}";
     }
 
-    public class PersonProperties
-    {
-        private string firstName;
-
-        private PersonProperties(string firstName)
-        {
-            firstName = firstName;
-        }
-
-        public static PersonProperties Of(string firstName)
-        {
-            if (firstName.Length <= 2)
-            {
-                return new PersonProperties(firstName);
-            }
-            else
-            {
-                throw new ArgumentOutOfRangeException("Imię jest za krótkie");
-            }
-        }
-
-        public string FirstName
-        {
-            get
-            {
-                return firstName;
-            }
-            set
-            {
-                if (value.Length >= 2)
-                {
-                    firstName = value;
-                }
-            }
-        }
-    }
-
-    public class PersonField
-    {
-        private string FirstName;
-        public string getFirstName()
-        {
-            return FirstName;
-        }
-        public void setFirstName(string value)
-        {
-            FirstName = value;
-        }
-    }
-
-    public enum Currency
-    {
-        PLN = 1,
-        USD = 2,
-        EUR = 3
-    }
-
-
-    public class Money
-    {
-        private readonly decimal _value;
-        
-        private readonly Currency _currency;
-        private Money(decimal value, Currency currency)
-        {
-            _value = value;
-            _currency = currency;
-        }
-
-    }
-    public decimal Value
-    {
-        get;
-    }
-
-
-    public static Money? Of(decimal value, Currency currency)
-    {
-        return value < 0 ? null : new Money(value, currency);
-    }
-
-    public static Money? OfWithException(decimal value, Currency currency)
+    public static Money OfWithException(decimal value, Currency currency)
     {
         if (value < 0)
         {
-            throw new ArgumentOutOfRangeException("Argument mniejszy niż 0");
+            throw new Exception("Error value<0");
         }
-        else
-        {
-            return new Money(value, currency);
-        }
+
+        return new Money(value, currency);
     }
 
-    public static Money operator*(Money money, decimal value)
+    public static Money ParseValue(string stringValue, Currency currency)
     {
-        return Money.Of(money.Value * value, money.Currency);
+        decimal value = decimal.Parse(stringValue);
+        return new Money(value, currency);
     }
-   
+
+    public bool Equals(Money? other)
+    {
+        if (other == null) return false;
+        if (_currency != other.Currency) return false;
+        return (_value == other.Value);
+    }
+
+    public Currency Currency
+    {
+        get { return _currency; }
+    }
+    public decimal Value
+    {
+        get { return _value; }
+    }
+    public static Money operator *(Money money, decimal factor)
+    {
+        return new Money(money.Value * factor, money.Currency);
+    }
+    public static Money operator +(Money moneya, Money moneyb)
+    {
+        if (moneya.Currency != moneyb.Currency) throw new Exception("Error Currencies not matching");
+        return new Money(moneya.Value + moneyb.Value, moneya.Currency);
+    }
+    public static bool operator <(Money moneya, Money moneyb)
+    {
+        if (moneya.Currency != moneyb.Currency) throw new Exception("Error Currencies not matching");
+        return (moneya.Value < moneyb.Value);
+    }
+    public static bool operator >(Money moneya, Money moneyb)
+    {
+        if (moneya.Currency != moneyb.Currency) throw new Exception("Error Currencies not matching");
+        return (moneya.Value > moneyb.Value);
+    }
+    public static explicit operator float(Money money)
+    {
+        return (float)money.Value;
+    }
+
 }
+public class Tank
+{
+    public readonly int Capacity;
+    private int _level;
+    public Tank(int capacity)
+    {
+        Capacity = capacity;
+    }
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            if (value < 0 || value > Capacity) throw new ArgumentOutOfRangeException();
+            _level = value;
+        }
+    }
+    public bool Consume(int w)
+    {
+        if (w > _level)
+        {
+            return false;
+        }
+        Level = _level - w;
+        return true;
+    }
+    public void Refuel(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException("Argument cant be non positive!");
+        }
+        if (_level + amount > Capacity)
+        {
+            throw new ArgumentException("Argument is to large!");
+        }
+        _level += amount;
+    }
+    public bool Refuel(Tank sourceTank, int amount)
+    {
+        if (this.Level + amount > Capacity)
+        {
+            return false;
+        }
+        if (sourceTank.Consume(amount))
+        {
+            this.Refuel(amount);
+            return true;
+        }
+
+        return false;
+    }
+
+}
+public static class MoneyExtension
+{
+    public static Money Percent(this Money money, decimal percent)
+    {
+        return Money.OfWithException((money.Value * percent) / 100m, money.Currency);
+    }
+    public static Money ToCurrency(this Money money, decimal factor, Currency curr)
+    {
+        return Money.OfWithException(money.Value * factor, curr);
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        var m1 = Money.OfWithException(10, Currency.USD);
+        var m2 = Money.OfWithException(15, Currency.USD);
+        Console.WriteLine(m1);
+        Console.WriteLine(m1 > m2);
+        Console.WriteLine(m1 == m2);
+        Console.WriteLine(m1 != m2);
+        var m3 = m1.Percent(50);
+        Console.WriteLine(m3);
+        var m4 = m1.ToCurrency(4, Currency.PLN);
+        Console.WriteLine(m4);
+    }
+}
+
