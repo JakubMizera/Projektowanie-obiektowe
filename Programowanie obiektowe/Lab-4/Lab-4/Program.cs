04021c5535d453d2771559b54b1950e611842ded
JakubMizera <kubamiz53@gmail.com>
2022-04-20 08:27:22 +0200

    lab3-6

---
+++ Programowanie obiektowe/Lab-4/Lab-4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lab_4
+{
+    internal class Program
+    {
+        // enum = stała wyliczeniowa
+        public enum Degree
+        {
+            A = 50,
+            B = 45,
+            C = 40,
+            D = 35,
+            E = 30,
+            F = 20,
+            G = 10
+        }
+
+        public static double Convert(Degree degree)
+        {
+            return degree switch
+            {
+                Degree.A => 5.0,
+                Degree.B => 4.5,
+                Degree.C => 4.0,
+                Degree.D => 3.5,
+                Degree.E => 3.0,
+                Degree.F => 2.0,
+                Degree.G => 1.0
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            Degree degree = Degree.A;
+            Console.WriteLine(degree);
+            Console.WriteLine((int)degree);
+            string [] names = Enum.GetNames<Degree>();
+            Console.WriteLine(names);
+            Degree[] degrees = Enum.GetValues<Degree>();
+            Array.Sort(degrees, (a, b) => -a.CompareTo(b));
+            
+            // zmiana kolejnosci z  g f e... na a b c ...
+            Console.WriteLine("Wpisz jedną z ocen");
+
+            foreach (var d in degrees)
+            {
+                Console.WriteLine(d);
+            }
+            string degreeString = Console.ReadLine();
+            try
+            {
+                Degree studentDegree = Enum.Parse<Degree>(degreeString);
+                Console.WriteLine($"Wpisałeś ocenę {studentDegree}");
+
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Wpisałeś nie poprawną ocenę");
+            }
+        }
+
+        public static string MessageFromDegree(Degree degree)
+        {
+            return degree switch
+            {
+                Degree.A or Degree.B or Degree.C or Degree.D or Degree.E => "Pozytywna",
+                Degree.F or Degree.G => "Negatywna",
+                _ => throw new ArgumentException("Zła ocena") // _ to wartość domyślna
+            };
+        }
+
+    }
+}
---
+++ Programowanie obiektowe/lab-06/lab-06/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_06
+{
+    internal class Program
+    {
+
+        class Student
+        {
+            public string Name { get; set; }
+            public int Ects { get; set; }
+
+
+            // metoda Equals aby działała funkcja Contains() dla kolekcji 
+            public override bool Equals(object obj)
+            {
+                Console.WriteLine("Student Equals");
+                return obj is Student student &&
+                       Name == student.Name &&
+                       Ects == student.Ects;
+            }
+
+            public override int GetHashCode()
+            {
+                Console.WriteLine("Student Hash");
+                return HashCode.Combine(Name, Ects);
+            }
+
+            public override string ToString()
+            {
+                return $"Name = {Name}, ETCS = {Ects}";
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            ICollection<string> names = new List<string>();
+            names.Add("Ewa");
+            names.Add("Tomek");
+            names.Add("Iga");
+            names.Add("Rafał");
+
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine(names.Contains("Ewa"));
+            
+            ICollection<Student> students = new List<Student>();
+            students.Add(new Student() { Name = "Ewa", Ects = 12});
+            students.Add(new Student() { Name = "Tomek", Ects = 23});
+         
+            Console.WriteLine(students.Contains(new Student() { Name = "Ewa", Ects = 12 }));
+
+
+
+            IList<Student> list = (IList<Student>)students;
+            Console.WriteLine(list[0]);
+            list.Insert(0, new Student() { Name = "Max", Ects = 16});  
+            
+            int i = list.IndexOf(new Student() { Name = "Tomek", Ects = 23 });
+            Console.WriteLine(i);
+
+            // klasa do tworzenia zbiorów
+            ISet<string> namesSet = new HashSet<string>();
+            namesSet.Add("Ewa");
+            namesSet.Add("Ewa");
+            namesSet.Add("Ewa");
+            //zbiór nie dopuszcza do dodania takiego samego 
+            Console.WriteLine(string.Join(",", namesSet));
+            // wypisze tylko raz "Ewa"
+
+
+
+            ISet<Student> studentGroup = new HashSet<Student>();
+            studentGroup.Add(new Student() { Name = "Ewa", Ects = 14 });
+            studentGroup.Add(new Student() { Name = "Ewa", Ects = 14 });
+            studentGroup.Add(new Student() { Name = "Ewa", Ects = 14 });
+            Console.WriteLine(string.Join(",", studentGroup));
+
+            Console.WriteLine(studentGroup.Contains(new Student() { Name = "Ewa", Ects = 14 }));
+
+            ISet<Student> team = new HashSet<Student>();
+            team.Add(new Student() { Name = "Ewa", Ects = 14 });
+            //czesc wspolna
+            studentGroup.IntersectWith(team);
+            Console.WriteLine(string.Join(",", studentGroup));
+
+
+
+            Dictionary<string, Person> addressBook = new Dictionary<string, Person>();
+            Person person = new Person() { Name = "karol", Email = "karol@gmail.com", Phone = "12341234" };
+            addressBook.Add(person.Phone, person);
+            person = new Person() { Name = "ewa", Email="ewa@gmail.com", Phone="43214321"};
+            addressBook.Add(person.Phone, person);
+            Console.WriteLine(addressBook["43214321"]);
+            Console.WriteLine(addressBook.Keys);
+            foreach(var item in addressBook)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+
+            string[] arr = { "dd", "aa", "cc", "ss", "aa", "dd", "r", "aa" };
+            //oblicz liczbe wystapien kazdego z lancuchow w arr; uzyc slownika
+            int[] set1 = { 19, 19, 63, 44, 15, 26, };
+            int[] set2 = { 25, 19, 63, 25, 15, 52, };
+            //wyznacz czesc wspolna tablic set1 set2
+            ISet<int[]> setOne = new HashSet<int[]>();
+            setOne.Add(set1);
+            ISet<int[]> setTwo = new HashSet<int[]>();
+            setTwo.Add(set2);
+            setOne.IntersectWith(setTwo);
+
+        }
+        class Person
+        {
+            // czym sie rozni String z wielkiej i string z małej litery?
+            public String Phone { get; set; }
+            public String Email { get; set; }
+            public String Name { get; set; }
+        }
+    }
+}
