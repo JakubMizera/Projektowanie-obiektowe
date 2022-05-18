9950f3d2ae8d707a50605bc4a0041d7dc9dc18d1
JakubMizera <kubamiz53@gmail.com>
2022-05-18 08:27:24 +0200

    previous labs

---
+++ Programowanie obiektowe/Lab-10/Lab-10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+   class Program {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        DatabaseContext context = new DatabaseContext();
+        context.Database.EnsureCreated();
+        //context.Students.Add(new Student { Name = "John", Age = 20 });
+        //context.SaveChanges();
+        IQueryable<Student> students = from s in context.Students
+                                       where s.Etcs > 20
+                  
+                                       select s;
+                                       foreach(Student s in students)
+                                        {
+                                            Console.WriteLine(s);
+                                        } 
+    }
+
+    public record Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Etcs { get; set; }
+    }
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+        
+        // onconfigure
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source=Database");
+        }
+        // onmodelcreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("students");
+        }
+    }
+}
